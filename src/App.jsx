0f7ae36e3ab77ca69ec53568aedcb8e7fb67a1df
sevans <sevans@highdesertmilk.com>
2023-11-03 12:47:05 -0600

    Finished components and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
-                // () instead of {} because this allows you to "imediately return what you want to return"
+import { CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality } from "./sections";
+import Nav from "./components/Nav";
+
+         // () instead of {} because this allows you to "imediately return what you want to return"
 const App = () => (
   <main className="relative">
-    Nav
+    <Nav/>
     <section className="xl:padding-1 wide:padding-r padding-b"> {/* padding-b defined in index.css */}
-      Hero
+      <Hero/>
     </section>
     <section className="padding">
-      PopularProducts
+      <PopularProducts/>
     </section>
     <section className="padding">
-      SuperQuality
+      <SuperQuality/>
     </section>
     <section className="padding-x py-10">
-      Services
+      <Services/>
     </section>
     <section className="padding">
-      SpecialOffer
+      <SpecialOffer/>
     </section>
     <section className="bg-pale-blue padding">
-      CustomerReviews
+      <CustomerReviews/>
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      Subscribe
+      <Subscribe/>
     </section>
     <section className="bg-black padding-x padding-t pb-8">
-      Footer
+      <Footer/>
     </section>
   </main>
 );
