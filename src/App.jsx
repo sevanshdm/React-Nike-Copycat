84288815e58615ff740ee23975ea929f0abdc464
sevans <sevans@highdesertmilk.com>
2023-11-03 14:41:24 -0600

    Finished Hero

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Nav from "./components/Nav";
 const App = () => (
   <main className="relative">
     <Nav/>
-    <section className="xl:padding-1 wide:padding-r padding-b"> {/* padding-b defined in index.css */}
+    <section className="xl:padding-l wide:padding-r padding-b"> {/* padding-b defined in index.css */}
       <Hero/>
     </section>
     <section className="padding">
